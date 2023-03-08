0d9fd22b4548de13161dd761bdaca129d3c8797b
Your Name <you@example.com>
2023-03-08 20:19:38 +0200

    Upload Design

---
+++ js/index.js
@@ -0,0 +1,16 @@
+// let input=document.querySelector(".Calculator .input");
+// let p=document.querySelectorAll(".box input");
+// p.forEach(p=>{
+//    p.addEventListener("click",function(){
+//    let r= input.value+= p.value
+//     if(p.value=='='){
+//         eval(p.value)
+
+
+// }
+
+// })
+
+// })
+
+// //  console.log( typeof 1) 
